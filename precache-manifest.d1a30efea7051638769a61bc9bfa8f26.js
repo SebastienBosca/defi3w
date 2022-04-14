35821547bf34f8669c02162c4b666c695b0fe10e
SebastienBosca <sbosca@wanadoo.fr>
2022-04-14 18:42:21 +0200

    Updates

---
+++ precache-manifest.d1a30efea7051638769a61bc9bfa8f26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37164cb8eca06993561bb36bf9e113b",
+    "url": "/defi3w/index.html"
+  },
+  {
+    "revision": "311ec366a4ab09ecd0ae",
+    "url": "/defi3w/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "a75d2925d570ccbeeb08",
+    "url": "/defi3w/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "311ec366a4ab09ecd0ae",
+    "url": "/defi3w/static/js/2.f596cccc.chunk.js"
+  },
+  {
+    "revision": "a75d2925d570ccbeeb08",
+    "url": "/defi3w/static/js/main.c8d1392f.chunk.js"
+  },
+  {
+    "revision": "c383d4e837708ba7aae5",
+    "url": "/defi3w/static/js/runtime-main.61a4e18e.js"
+  }
+]);
